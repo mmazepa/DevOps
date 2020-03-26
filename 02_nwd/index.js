cabc23efa5bd3c54a5702495868364ca14487981
mmazepa <maksymilianmariusz@gmail.com>
2020-03-26 09:55:33 +0100

    NWD.

---
+++ 02_nwd/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
+
+function nwd (a, b) {
+  var q;
+  while (b != 0) {
+    q = a;
+    a = b;
+    b = q%b;
+  }
+  return a;
+}
+
+app.get('/:n1/:n2', function (req, res) {
+  const val1 = req.params.n1;
+  const val2 = req.params.n2;
+
+  console.log('num1: ' + val1 + ', num2: ' + val2);
+
+  const pair = req.params.val1 + "," + req.params.val2;
+
+  client.get(pair, function (err, result) {
+    if (result) {
+      res.send('nwd(' + val1 + ',' + val2 + ') = ' + result + '\n');
+    } else {
+      result = nwd(val1, val2);
+      client.set(pair, result);
+      res.send('nwd(' + val1 + ',' + val2 + ') = ' + result + '\n');
+    }
+  });
+});
+
+app.listen(8081, function () {
+  console.log('Listening on port 8081.');
+});
