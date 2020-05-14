ff38b22f53ad2d54dfb3126bebeab94e131b9575
mmazepa <maksymilianmariusz@gmail.com>
2020-05-14 09:52:43 +0200

    Backend, obsługa cache.

---
+++ 09_fragment_clustera/backend/index.js
@@ -0,0 +1,37 @@
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const redis = require('redis');
+
+const app = express();
+app.use(bodyParser.json());
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  retry_strategy: () => 1000
+});
+
+const appId = uuidv4();
+const appPort = 3000;
+
+const appName = `AppId: ${appId}`;
+app.get('/:key', (req, res) => {
+  const { key } = req.params;
+  redisClient.get(key, (err, value) => {
+    if (err) {
+      console.log(err);
+      return res.send(`${appName}: Wystąpił błąd!`);
+    }
+    if (value !== null) {
+      return res.send(`${appName}: Znaleziono w cache!`);
+    }
+    redisClient.set(key, "");
+    return res.send(`${appName}: Dodano do cache!`);
+  });
+});
+
+app.listen(appPort, () => {
+  console.log(`Backend listening on port ${appPort}`);
+});
