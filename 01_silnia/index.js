a17059e3b231b8575d455cf579590e20f27ed22b
mmazepa <maksymilianmariusz@gmail.com>
2020-06-01 00:03:48 +0200

    Niewielkie poprawki zadania z silnią, kwestie wydruku.

--- 01_silnia/index.js
+++ 01_silnia/index.js
@@ -7,34 +7,41 @@ const app = express();
 const process = require('process');
 
 const client = redis.createClient({
-	host: 'redis-server',
-	port: 6379
+  host: 'redis-server',
+  port: 6379
 });
 
 const factorial = (n) => {
-	if (n == 0) {
-		client.set(n, 1);
-		return 1;
-	} else {
-		const result = (n * factorial(n-1));
-		client.set(n, result);
-		return result;
-	}
+  if (n == 0) {
+    client.set(n, 1);
+    return 1;
+  } else {
+    const result = (n * factorial(n-1));
+    client.set(n, result);
+    return result;
+  }
 };
 
 app.get('/:number', (req, resp) => {
-	const value = req.params.number;
-	if (value >= 10) process.exit(1);
+  const value = req.params.number;
 
-	client.get(value, (err, result) => {
-		if (result == null || result == undefined)
-			result = factorial(value);
-		console.log('REQ: ' + value + ', RESP: ' + result);
-		const answer = 'silnia(' + value + ') = ' + result;
-		resp.send(answer + '\n');
-	});
+  if (value < 0) {
+    resp.send('Za mała liczba. Restart...\n');
+    process.exit(1);
+  } else if (value >= 10) {
+    resp.send('Za duża liczba. Restart...\n');
+    process.exit(1);
+  }
+
+  client.get(value, (err, result) => {
+    if (result == null || result == undefined)
+      result = factorial(value);
+    console.log('REQ: ' + value + ', RESP: ' + result);
+    const answer = 'silnia(' + value + ') = ' + result;
+    resp.send(answer + '\n');
+  });
 });
 
 app.listen(8080, () => {
-	console.log('Server listening on port 8080.');
+  console.log('Server listening on port 8080.');
 });
