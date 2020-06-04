b090a4b8f006bb8d7ed0d5974e4d8676302b772c
mmazepa <maksymilianmariusz@gmail.com>
2020-06-04 03:53:24 +0200

    M.in. initMessage w keys.js

--- 10_cluster/backend/index.js
+++ 10_cluster/backend/index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const redis = require('redis');
+const keys = require('./keys');
 
 const app = express();
 app.use(bodyParser.json());
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  host: keys.redisHost,
+  port: keys.redisPort,
   retry_strategy: () => 1000
 });
 
@@ -17,7 +18,7 @@ const appId = uuidv4();
 const appPort = 5000;
 
 app.get('/', (req, res) => {
-  return res.send(`[${appId}] Hello from backend app!`);
+  return res.send(`[${appId}] ${keys.initMessage}`);
 });
 
 app.listen(appPort, () => {
